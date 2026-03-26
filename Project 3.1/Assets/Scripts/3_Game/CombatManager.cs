33b149be0bfea1f37ff77103a720a8c0afee0d1f
iann <ianathanwong@gmail.com>
2026-03-25 18:33:22 -0700

    Phase Change Threshold Variable

--- Project 3.1/Assets/Scripts/3_Game/CombatManager.cs
+++ Project 3.1/Assets/Scripts/3_Game/CombatManager.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
 public class CombatManager : MonoBehaviour
 {
+    [Header("Main Enemy")]
     [SerializeField] private GameObject enemy;
     [SerializeField] private Transform enemySpawn;
     private Enemy _enemy;
+
+    [Header("Combat Variables")]
+    [SerializeField] [Range(0f, 1f)] private float phaseChangeThreshold = 0.25f;    // % of DEF the enemy has before changing combat states
 
     void Awake()
     {
