5614f0905e35f08736671a8641c84517fe8dcd3e
iann <ianathanwong@gmail.com>
2026-03-31 14:12:40 -0700

    New EnemyAttack Parent ScriptableObject

---
+++ Project 3.1/Assets/Scripts/2_Enemy/EnemyAttack.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+public abstract class EnemyAttack : ScriptableObject
+{
+    [Header("Attack Info")]
+    public string attackName;
+    public int attackID;
+
+    [Header("Attack Selection Settings")]
+    public float baseWeight = 1f;
+    public float weightIncrement = 0.5f;        // how much to gain when not used
+    public float weightDecrement = 2f;          // how much to lose when used
+    [NonSerialized] public float currentWeight; // current weight value of THIS attack
+
+    void OnEnable() => currentWeight = baseWeight;
+
+    public void IncreaseWeight() => currentWeight += weightIncrement;
+    public void DecreaseWeight() => currentWeight = Mathf.Max(baseWeight - weightDecrement, 0f);
+
+    // Attack implementation done by each attack
+    public abstract void Attack();
+}
+
+public abstract class EnemyRangedAttack : EnemyAttack {}
+public abstract class EnemyFocusAttack  : EnemyAttack {}
+public abstract class EnemyMeleeAttack  : EnemyAttack {}
+public abstract class EnemyZoneAttack   : EnemyAttack {}
